ed79f40022d0d419265b667dcfcc005a59ea0dc7
louiserun <louise.runavot@gmail.com>
2026-08-03 17:03:23 +0200

    fix app.ts/app.html pour utiliser NavigationService

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { Library } from './features/library/library/library';
 import { BookLookup } from './features/book-lookup/book-lookup/book-lookup';
+import { Navigation } from './core/services/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Library, BookLookup],
+  imports: [Library, BookLookup],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('book-tracker');
+  nav = inject(Navigation);
+
+  setTab(tab: 'library' | 'search') {
+    this.nav.setTab(tab);
+  }
 }
